63585251a218f1dde6668e4005d9b6eea831af22
writerben <64561235+writerben@users.noreply.github.com>
2020-08-07 13:52:41 +0100

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -61,7 +61,7 @@ export default {
   disqus: {
     on: true,
     loadingStrategy: 'button', // Options: onload, lazy, button
-    siteShortName: 'blog-danielkelly-io' // 'blog-danielkelly-io'
+    siteShortName: 'vacancypool' // 'blog-danielkelly-io'
   },
 
   // Newsletter Subscribe
